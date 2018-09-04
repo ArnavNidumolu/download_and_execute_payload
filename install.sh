11e255b2cd2e943e99769af6008e27b0cf02d15c
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2018-09-04 10:53:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ rm /var/lib/apt/lists/lock
 sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get install -y wine32 python-pip pyinstaller
-wget httpswww.python.orgftppython2.7.14python-2.7.14.msi
+wget https://www.python.org/ftp/python/2.7.14/python-2.7.14.msi
 wine msiexec i python-2.7.14.msi
-sudo wine ~.winedrive_cPython27python.exe -m pip install requests==2.5.1
+sudo wine ~/.wine/drive_c/Python27/python.exe -m pip install -m pip install requests==2.5.1
 sudo pip install requests==2.5.1
