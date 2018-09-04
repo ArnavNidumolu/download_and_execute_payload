e34e8f3c77bc5f3c1d7ddc31a0babef93ce93b14
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2018-09-04 10:57:46 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ rm /var/cache/apt/archives/lock
 rm /var/lib/apt/lists/lock
 sudo dpkg --add-architecture i386
 sudo apt-get update
-sudo apt-get install python-pip
-sudo apt-get install wine-stable
+sudo apt-get install -y python-pip
+sudo apt-get install -y wine-stable
 sudo apt-get install -y wine32 python-pip pyinstaller
 wget https://www.python.org/ftp/python/2.7.14/python-2.7.14.msi
 wine msiexec i python-2.7.14.msi
